035becef3ecd7256d292c79c1034bf1f17f5c035
Thomas <thomas@mbp-de-thomas.home>
2022-06-20 17:36:22 +0200

    test heroku

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(`${__dirname}/dist`));
-// Start the app by listening on the default
-// Heroku port
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/botmind-test-angular'));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/botmind-test-angular/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
